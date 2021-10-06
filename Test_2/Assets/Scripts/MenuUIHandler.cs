6edece65cdb5f5f517f5c5be00b6a1d46a125209
ilia_sokolov <ilia_sokolov90@mail.ru>
2021-10-06 16:48:20 +0300

    Some Change 2

--- Test_2/Assets/Scripts/MenuUIHandler.cs
+++ Test_2/Assets/Scripts/MenuUIHandler.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -11,10 +13,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuUIHandler : MonoBehaviour
 {
+    private AudioSource menuAudio;
+    bool audioState = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuAudio = GetComponent<AudioSource>();
     }
 
     public void StartNew()
@@ -22,8 +27,28 @@ public class MenuUIHandler : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    public void BackToMenu()
+    public void MenuAudioSource()
     {
-        SceneManager.LoadScene(0);
+        if (audioState)
+        {
+            menuAudio.Stop();
+            audioState = false;
+        }
+        else
+        {
+            menuAudio.Play();
+            audioState = true;
+        } 
     }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+    
 }
